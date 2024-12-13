a42c82386b7f672732f818bdee9e72a263df74ca
Vamsi Munagala <Vamsi.Munagala@hotmail.com>
2024-12-13 15:32:46 -0500

    data ingestion part is complete

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     exc_type, exc_obj, exc_tb = error_detail.exc_info()
