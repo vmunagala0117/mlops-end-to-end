cf03c691cd59ed7a2829d96eb485ab1ef0ada09f
Vamsi Munagala <Vamsi.Munagala@hotmail.com>
2024-12-17 13:43:55 -0500

    flask webapp endpoint

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,6 @@ import sys
 
 import numpy as np
 import pandas as pd
-import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -54,6 +53,16 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
         return report
 
+    except CustomException as ce:
+        logging.error(ce)
+        print(str(ce))
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        
     except CustomException as ce:
         logging.error(ce)
         print(str(ce))
